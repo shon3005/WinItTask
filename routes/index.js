88c8ce02101ce8f00d299aa4a1ce5801160adf3c
Shaun Chua <shon3005@gmail.com>
2017-05-26 01:41:42 -0400

    setup changes bugs

--- routes/index.js
+++ routes/index.js
@@ -13,17 +13,6 @@ router.get('/helloworld', function(req, res) {
     res.render('helloworld', { title: 'Hello, World!' });
 });
 
-/* GET Userlist page. */
-// router.get('/userlist', function(req, res) {
-//     var db = req.db;
-//     var collection = db.get('usercollection');
-//     collection.find({},{},function(e,docs){
-//         res.render('emaillist', {
-//             "userlist" : docs
-//         });
-//     });
-// });
-
 /* GET New User page. */
 router.get('/newmessage', function(req, res) {
     res.render('newmessage', { title: 'Add New Message' });
